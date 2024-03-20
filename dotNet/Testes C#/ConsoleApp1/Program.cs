25825580a27a27746eada9a36be2bdeede026b6e
EDUARDO-NITRO5\Eduardo <eduardolimadoi@gmail.com>
2024-03-20 12:33:36 -0300

    first commit

---
+++ dotNet/Testes C#/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+
+            int A = int.Parse(vet[0], CultureInfo.InvariantCulture);
+            int B = int.Parse(vet[1], CultureInfo.InvariantCulture);
+            int C = int.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            int MaiorAb = (A+B+Math.Abs(A-B))/2;
+            int MaiorC = (MaiorAb+ C+Math.Abs(MaiorAb-C))/2;
+
+            Console.WriteLine(MaiorC+" eh o maior");
+            Console.WriteLine();
+        }
+    }
+}
+
